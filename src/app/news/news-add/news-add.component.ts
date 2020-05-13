3160c8dd14b0af8d69e445a6555993a8be85c750
Robin <robin.berg@hotmail.nl>
2020-05-13 15:32:37 +0200

    added logic for the news-add component

--- src/app/news/news-add/news-add.component.ts
+++ src/app/news/news-add/news-add.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { action } from "tns-core-modules/ui/dialogs";
 
 @Component({
   selector: 'ns-news-add',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./news-add.component.css']
 })
 export class NewsAddComponent implements OnInit {
-
+  userType = " ";
   constructor() { }
-
   ngOnInit(): void {
   }
+  displayActionDialog() {
+    const options = {
+      title: "Gebruiker",
+      cancelButtonText: "annuleren",
+      actions: ["Mijzelf", "Bedrijf"]
+    };
 
+    action(options).then((result) => {
+      if (result === "annuleren") {
+        this.userType = "";
+      } else {
+        this.userType = result;
+      }
+    });
+  }
 }
